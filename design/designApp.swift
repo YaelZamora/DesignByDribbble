c3fddc66c03443e3cb55ebad505b08734485a848
YaelZamora <ajedrezkasparov@gmail.com>
2023-05-09 12:18:02 -0500

    Initial Commit

---
+++ design/designApp.swift
@@ -0,0 +1,17 @@
+//
+//  designApp.swift
+//  design
+//
+//  Created by MacMini on 09/05/23.
+//
+
+import SwiftUI
+
+@main
+struct designApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
